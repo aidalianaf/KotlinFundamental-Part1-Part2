29a430e9b2b36cb9386ca4959b6024355cd086a6
aidalianaf <example@mail.com>
2020-08-29 19:43:12 +0700

    pesan commit

---
+++ src/main/kotlin/1.Hello-kotlin.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val name = "Aida Liana"
+    val hobi = "makan"
+
+    print("nama saya : ")
+    println(name)
+    print("hobi saya : ")
+    println(hobi)
+}
---
+++ src/main/kotlin/2.Data-types.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val firstword = "SMK "
+    val middleword = "Telkom "
+    val lastword = "Purwokerto "
+    println(firstword + middleword + lastword)
+
+    val panjang : Int = 43
+    val lebar = 2
+    val teks = "Luas Persegi Panjang : "
+    print(teks)
+    print(panjang * lebar )
+
+}
